c1bfb0bf14ca7a6ab6edbc6cf2255024fcb4fe24
timkoller <tim@koller.app>
2025-02-05 15:58:42 +0100

    test.html: added progress bar

--- test/script.js
+++ test/script.js
@@ -1,30 +1,57 @@
-function startCounting() {
-    const inputDate = document.getElementById('dateInput').value;
-    if (!inputDate) {
-        alert("Please enter a date!");
-        return;
+const app = new Vue({
+    el: "#app",
+    data() {
+      return {
+        colors: ["#D53738", "#638867", "#FAF429"],
+        current: 0
+      };
+    },
+    mounted() {
+      let timeline = anime.timeline({
+        autoplay: true,
+        duration: 10000,
+        easing: "linear",
+        loop: true
+      });
+  
+      this.colors.forEach((color, index) => {
+        timeline.add({
+          targets: document.querySelectorAll("nav > div")[index].children[0],
+          width: "100%",
+          changeBegin: (a) => {
+            this.current = index;
+          }
+        });
+      });
+  
+      let hammertime = new Hammer(document.querySelector("#app"));
+  
+      hammertime.on("press", (e) => {
+        timeline.pause();
+      });
+  
+      hammertime.on("pressup", (e) => {
+        timeline.play();
+      });
+  
+      hammertime.on("tap", (e) => {
+        if (e.center.x > window.innerWidth / 2) {
+          if (this.current < this.colors.length - 1) {
+            this.current += 1;
+  
+            timeline.pause();
+            timeline.seek(this.current * 10000);
+            timeline.play();
+          }
+        } else {
+          if (this.current > 0) {
+            this.current -= 1;
+  
+            timeline.pause();
+            timeline.seek(this.current * 10000);
+            timeline.play();
+          }
+        }
+      });
     }
-
-    const startDate = new Date(inputDate);
-    
-    function updateCounter() {
-        const now = new Date();
-        const diffInSeconds = Math.floor((now - startDate) / 1000);
-        
-        const seconds = diffInSeconds;
-        const minutes = Math.floor(seconds / 60);
-        const hours = Math.floor(minutes / 60);
-        const days = Math.floor(hours / 24);
-        const weeks = Math.floor(days / 7);
-        const months = Math.floor(days / 30.44);
-
-        document.getElementById('months').textContent = months;
-        document.getElementById('weeks').textContent = weeks;
-        document.getElementById('days').textContent = days;
-        document.getElementById('hours').textContent = hours;
-        document.getElementById('minutes').textContent = minutes;
-        document.getElementById('seconds').textContent = seconds;
-    }
-
-    setInterval(updateCounter, 1000);
-}
+});
